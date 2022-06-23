b7a19fcaf2eed057dac6502999d159ba0e997f5a
JersyFernandesJF <jersysilvafernandes@gmail.com>
2022-06-23 23:18:57 +0100

    finished now kkk

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -2,11 +2,12 @@ import './Nav.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default props => 
+export default props =>
     <aside className="menu-area">
         <nav className="menu">
+            {/* Refatorar em casa! */}
             <Link to="/">
-                <i className="fa fa-home"></i> Inicio
+                <i className="fa fa-home"></i> Início
             </Link>
             <Link to="/users">
                 <i className="fa fa-users"></i> Usuários
